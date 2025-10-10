cfaad0032c36bb5dcc59111df0860079e2b26ca0
Oyebowale Azeez <Oyebowale@relianceinfosystems.com>
2025-10-11 00:20:38 +0100

    public upload

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
